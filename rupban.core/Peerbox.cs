498393e2bfb1f4fab9e4092f20fdfff55cc44112
nahidupa <nahidupa@d36bf5aa-001f-07b2-f3bd-3af3220328e8>
2010-04-04 17:29:11 +0000

    basic design

---
+++ rupban.core/Peerbox.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace rupban.core
+{
+    public class Peerbox : TemplateCel
+    {
+        public Resource Resources
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+}
