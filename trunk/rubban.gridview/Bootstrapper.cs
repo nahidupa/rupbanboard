f3148ee6f89fa23764463bc88a321508a50a6d0e
nahidupa <nahidupa@d36bf5aa-001f-07b2-f3bd-3af3220328e8>
2010-04-19 05:25:36 +0000

    #add prism

---
+++ trunk/rubban.gridview/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using Microsoft.Practices.Composite.UnityExtensions;
+
+namespace rubban.gridview
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            Shell shell = this.Container.Resolve<Shell>();
+            shell.Show();
+            return shell;
+        }
+
+        protected override void InitializeModules()
+        {
+            //IModule employeeModule = this.Container.Resolve<EmployeeModule>();
+            //employeeModule.Initialize();
+
+            //IModule projectModule = this.Container.Resolve<ProjectModule>();
+            //projectModule.Initialize();
+        }
+    }
+}
