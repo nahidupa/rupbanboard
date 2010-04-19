07a82103ac125aca02ba0f51295fd75bd79060ae
nahidupa <nahidupa@d36bf5aa-001f-07b2-f3bd-3af3220328e8>
2010-04-19 13:21:14 +0000

    #adding unity

---
+++ rupban.loginmodule/Presenters/LoginPresenter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using rupban.loginmodule.Controller;
+using rupban.loginmodule.Views;
+
+namespace rupban.loginmodule.Presenters
+{
+    public class LoginPresenter : ILoginPresenter
+    {
+        private ILoginController _loginController;
+
+        public LoginPresenter(ILoginView view,ILoginController loginController)
+        {
+            _loginController = loginController;
+            View = view;
+        }
+
+        public ILoginView View
+        {
+            get ;
+            set ; 
+        }
+    }
+}
---
+++ rupban.ui.infrastructure/UnityContainerExtention.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.Unity;
+
+namespace rupban.ui.infrastructure
+{
+    public static class UnityContainerExtention
+    {
+        private static Dictionary<Type, Type> _registeredTypes;
+
+        public static Dictionary<Type, Type> RegisteredTypes
+        {
+            get
+            {
+                if (_registeredTypes == null)
+                    _registeredTypes = new Dictionary<Type, Type>();
+                return _registeredTypes;
+            }
+        }
+
+        public static void RegisterType(this IUnityContainer source, Type fromType, Type toType, bool registerAsSingleton)
+        {
+            if (RegisteredTypes.ContainsKey(fromType) == false)
+            {
+                RegisteredTypes.Add(fromType, toType);
+            }
+            if (registerAsSingleton)
+            {
+                source.RegisterType(fromType, toType, new ContainerControlledLifetimeManager());
+            }
+            else
+            {
+                source.RegisterType(fromType, toType);
+            }
+        }
+        public static bool IsRegisteredType(this IUnityContainer source, Type fromType)
+        {
+            return RegisteredTypes.ContainsKey(fromType);
+        }
+
+        public static void RegisterInstance(this IUnityContainer source, Type type, object instance, bool registerAsSingleton)
+        {
+            if (registerAsSingleton)
+            {
+                source.RegisterInstance(type, instance, new ContainerControlledLifetimeManager());
+            }
+            else
+            {
+                source.RegisterInstance(type, instance);
+            }
+        }
+    }
+}
