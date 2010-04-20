f4d02a1f0568eea72c69899f8fe90a546e041bb1
nahidupa <nahidupa@d36bf5aa-001f-07b2-f3bd-3af3220328e8>
2010-04-20 09:41:10 +0000

    #add TicketUI

---
+++ rupban.loginmodule/Views/ITicketView.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace rupban.loginmodule.Views
+{
+    public interface ITicketView
+    {
+    }
+}
